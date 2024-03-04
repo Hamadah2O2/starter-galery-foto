73d5b937d8fb9b25bc93faca2b8575a43d9c3007
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-03-04 08:59:40 +0700

    initial 3

--- app/Views/layout/base.php
+++ app/Views/layout/base.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css" />
 </head>
+
 <body>
     <?= $this->renderSection('content') ?>
 
     <script src="<?= base_url() ?>assets/js/bootstrap.js"></script>
     <script src="<?= base_url() ?>assets/js/bootstrap.bundle.js"></script>
 </body>
+
 </html>
